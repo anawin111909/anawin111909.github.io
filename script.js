75934be76f009c28029b10083bd6f14f4801f487
anawin111909 <108145027+anawin111909@users.noreply.github.com>
2026-03-14 15:18:32 +0000

    Update4.0
    Add exp. projects

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ el.classList.add("active");
 }
 
 window.addEventListener("scroll", revealOnScroll);
+
+/* Project1  */
+const slider = document.querySelector(".media-slider")
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+
+let index = 0
+
+next.onclick = () =>{
+    index++
+    if(index > 3) index = 0
+    slider.style.transform = `translateX(-${index * 100}%)`
+}
+
+prev.onclick = () =>{
+    index--
+    if(index < 0) index = 3
+    slider.style.transform = `translateX(-${index * 100}%)`
+}
